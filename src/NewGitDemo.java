27fc8307b6f4c61d05b56a03a205e43d868a65ae
Vince Beekel <vincebeekel@gmail.com>
2018-01-23 14:43:01 -0500

    first commiit

---
+++ src/NewGitDemo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NewGitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Scanner scnr = new Scanner(System.in);
+	String name;
+	
+	System.out.println("What is your name?");
+	name = scnr.nextLine();
+	System.out.println(name);
+	
+	
+	
+	}
+
+}
